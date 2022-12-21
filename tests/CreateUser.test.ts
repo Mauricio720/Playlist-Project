d1f78629ce96c1a735a7fa992306a1163bba74ab
Mauricio Ferreira <mauricio-ferreira2015@outlook.com>
2022-12-20 23:29:13 -0300

    test: create user

--- tests/CreateUser.test.ts
+++ tests/CreateUser.test.ts
@@ -1,3 +1,45 @@
 import { User } from "domain/entities/User";
+import { CryptoIdentifier } from "infra/security/CryptoIdentifier";
+import { Identifier } from "infra/security/Identifier";
+import { Encrypt } from "infra/security/Encrypt";
+import { CreateUser } from "application/useCases/CreateUser";
+import assert from "assert";
+import { UserRepositoryMemory } from "infra/repositories/memory/UserRepositoryMemory";
 
-describe("Create One User", () => {});
+describe("Create User", () => {
+    const userRepository=new UserRepositoryMemory()
+    
+    const identifier:Identifier={
+        createId(){
+            return "1"
+        }
+    }
+    
+    const encrypt: Encrypt = {
+        compare(password, encripted) {
+          return `${password}_enc` === encripted;
+        },
+        encript(password) {
+          return `${password}_enc`;
+        },
+    };
+    
+    it("should create user",async ()=>{
+        const createUser=new CreateUser(identifier,encrypt,userRepository)
+        const user=await createUser.execute({
+            name:"any",
+            email: "any@any.com",
+            password: "any",
+            favoriteCategory: [
+                { id: "any", name: "rock" },
+                { id: "any", name: "rap" },
+              ],
+            favoriteArtist: [{ id: "any", name: "any" }],
+        })
+
+        assert.deepEqual(user.id,'1')
+        assert.deepEqual(user.password,'any_enc')
+        assert.deepEqual(user.dateRegister.toDateString(),new Date().toDateString())
+
+    })
+});
