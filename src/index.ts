01137ad4a32c9ced600a23ae05c32245fec7c472
Mauricio Ferreira <mauricio-ferreira2015@outlook.com>
2022-12-28 18:54:02 -0300

    refactory: configs server and project

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,35 @@ import { UserRepositoryMemory } from "infra/repositories/memory/UserRepositoryMe
 import { BcryptAdapter } from "infra/security/BcryptAdapter";
 import { CryptoIdentifier } from "infra/security/CryptoIdentifier";
 import { UserController } from "infra/controllers/UserController";
-
-
+import passport from "passport";
+import { PassportMidleware } from "infra/http/middleware/PassportMiddleware";
+import { StrategyJwt } from "infra/security/StrategyJwt";
+import { JsonWebTokenAdapter } from "infra/security/JsonWebToken";
+import { AuthController } from "infra/controllers/AuthController";
 
 dotenv.config();
 
 const server = new ExpressServer();
 server.static("public");
+
 server.registerMiddleware(cors({origin:"*"}))
+server.registerMiddleware(passport.initialize())
 
 const userRepository=new UserRepositoryMemory()
 const encript=new BcryptAdapter()
 const identifier=new CryptoIdentifier()
 
-new UserController(server,userRepository,identifier,encript)
+const strategyJwt=new StrategyJwt(userRepository)
+const passportMidleware=new PassportMidleware(strategyJwt)
+
+server.registerMiddleware((req,res,next)=>{
+  passportMidleware.handle(req,res,next)
+})
+
+const jsonWebTokenAdapter = new JsonWebTokenAdapter();
+
+new UserController(server,userRepository,identifier,encript,jsonWebTokenAdapter)
+new AuthController(server,userRepository,jsonWebTokenAdapter,encript)
 
 server.listen(parseInt(process.env.PORT as string), () =>
   console.log(`Server listening on port ${process.env.PORT}`)
--- src/infra/security/AuthenticatorAdapter.ts
+++
@@ -1,9 +0,0 @@
-import { Authenticator } from "./Authenticator";
-
-export class AuthenticatorAdapter implements Authenticator{
-    createToken(payload: any){
-        return 'any'
-    }
-    
-    decoder(token: string){}
-}
