56aa6c4ef1eae620e3fbba9855c193bb17e1285f
Mauricio Ferreira <mauricio-ferreira2015@outlook.com>
2022-12-29 16:09:10 -0300

    test: update user

---
+++ tests/UpdateUser.test.ts
@@ -0,0 +1,113 @@
+import { CreateUser } from "application/useCases/CreateUser";
+import { UserRepositoryMemory } from "infra/repositories/memory/UserRepositoryMemory";
+import { Encrypt } from "infra/security/Encrypt";
+import { Identifier } from "infra/security/Identifier";
+import { Authenticator } from "infra/security/Authenticator";
+import { User } from "domain/entities/User";
+import assert from "assert";
+import { UpdateUser } from "application/useCases/UpdateUser";
+import { UserExists } from "domain/errors/UserExists";
+import { UserNotFound } from "domain/errors/UserNotFound";
+
+describe("Update User",async ()=>{
+    const userRepository=new UserRepositoryMemory()
+    
+    const identifier:Identifier={
+        createId(){
+            return "1"
+        }
+    }
+    
+    const encrypt: Encrypt = {
+        async compare(password, encripted) {
+          return `${password}_enc` === encripted;
+        },
+        encript(password) {
+          return `${password}_enc`;
+        },
+    };
+    
+    const authenticador: Authenticator = {
+        createToken() {
+            return "any token";
+        },
+        decoder() {},
+    };
+    
+
+    const createUser=new CreateUser(identifier,encrypt,authenticador,userRepository)
+    const userData:Omit<User.Props,'id'>={
+        name:"any",
+        email: "any@any.com",
+        password: "any",
+        favoriteCategory: [
+            { id: "any", name: "rock" },
+            { id: "any", name: "rap" },
+          ],
+        favoriteArtist: [{ id: "any", name: "any" }]
+    }
+
+    const {user}=await createUser.execute(userData)
+
+    it("should update user",async ()=>{
+        const updateUser=new UpdateUser(userRepository,encrypt)
+        const userModify=await updateUser.execute({
+            id:user.id,
+            name:"anyEdit",
+            email:"anyEdit@anyEdit.com",
+            password: "anyEdit",
+            favoriteCategory: [
+                { id: "any", name: "rock" },
+                { id: "any", name: "hip-hop" },
+                { id: "any", name: "eletronica" },
+              ],
+            favoriteArtist: [{ id: "any", name: "any" },{ id: "any", name: "any2" }]
+        })
+        
+        assert.deepEqual(userModify.id,user.id)
+        assert.deepEqual(userModify.name,"anyEdit")
+        assert.deepEqual(userModify.email,"anyEdit@anyEdit.com")
+        assert.deepEqual(userModify.password,'anyEdit_enc')
+        assert.deepEqual(userModify.favoriteCategory.length,3)
+        assert.deepEqual(userModify.favoriteCategory[2].name,"eletronica")
+        assert.deepEqual(userModify.favoriteArtist.length,2)
+    })
+    
+    it("throw error user not found",async ()=>{
+        const updateUser=new UpdateUser(userRepository,encrypt)
+
+        assert.rejects(async () => {
+            await updateUser.execute({
+            id:'not found',
+            name:"anyEdit",
+            email:"any@any.com",
+            password: "anyEdit",
+            favoriteCategory: [
+                { id: "any", name: "rock" },
+                { id: "any", name: "hip-hop" },
+                { id: "any", name: "eletronica" },
+              ],
+            favoriteArtist: [{ id: "any", name: "any" },{ id: "any", name: "any2" }]
+        })
+            }, new UserNotFound());
+    })
+
+    it("throw error user already exists",async ()=>{
+        const updateUser=new UpdateUser(userRepository,encrypt)
+
+        assert.rejects(async () => {
+            await updateUser.execute({
+            id:user.id,
+            name:"anyEdit",
+            email:"any@any.com",
+            password: "anyEdit",
+            favoriteCategory: [
+                { id: "any", name: "rock" },
+                { id: "any", name: "hip-hop" },
+                { id: "any", name: "eletronica" },
+              ],
+            favoriteArtist: [{ id: "any", name: "any" },{ id: "any", name: "any2" }]
+        })
+            }, new UserExists());
+    })
+})
