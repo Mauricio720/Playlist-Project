5dceab0aeb889f95fb497ac72921a02578c410b6
Mauricio Ferreira <mauricio-ferreira2015@outlook.com>
2022-12-29 16:20:49 -0300

    refactory: category

--- src/domain/entities/Category.ts
+++ src/domain/entities/Category.ts
@@ -3,6 +3,7 @@ import { FieldMissing } from "domain/errors/FieldMissing";
 export class Category {
   public readonly id: string;
   public name: string;
+  public icon?: string;
 
   constructor(props: Category) {
     Object.assign(this, props);
