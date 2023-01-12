a75d524b9928cfe231853965f982e04157000017
Mauricio Ferreira <mauricio-ferreira2015@outlook.com>
2023-01-11 23:13:48 -0300

    test: playlist and create playlist test

---
+++ tests/CreatePlaylist.test.ts
@@ -0,0 +1,114 @@
+import { CreatePlaylist } from "application/useCases/CreatePlaylst"
+import assert from "assert"
+import { Playlist } from "domain/entities/Playlist"
+import { PlaylistRepositoryMemory } from "infra/repositories/memory/PlaylistRepository"
+import { Identifier } from "infra/security/Identifier"
+import { SongRepositoryMemory } from "infra/repositories/memory/SongRepositoryMemory"
+import { SongNotFound } from "domain/errors/SongNotFound"
+import { Song } from "domain/entities/Song"
+import { UserRepositoryMemory } from "infra/repositories/memory/UserRepositoryMemory"
+import { User } from "domain/entities/User"
+
+describe("should create playlist",async ()=>{
+    const identifier:Identifier={
+        createId(){
+            return "1"
+        }
+    }
+    
+    const INITIAL_VALUES:Playlist.Props={
+        id: "1",
+        title: "any",
+        user: {
+            id:"any",
+            name:"any",
+            email:"any"
+        },
+        songs:[
+            {
+                id:"any",
+                title:"any",
+                category:{
+                    id:"any",
+                    name:"any",
+                },
+                duration:1.0,
+                pathSongFile:"any",
+                artist:{
+                    id:"any",
+                    name:"any",
+                    picture:"any"
+                },
+                album:{
+                    id:"any",
+                    name:"any",
+                    year:"any",
+                    cover:"any",
+                },
+                user:{
+                    id:"any",
+                    name:"any",
+                    email:"any@any.com",
+                }
+            },
+
+            {
+                id:"any",
+                title:"any2",
+                category:{
+                    id:"any",
+                    name:"any",
+                },
+                duration:2.0,
+                pathSongFile:"any",
+                artist:{
+                    id:"any",
+                    name:"any",
+                    picture:"any"
+                },
+                album:{
+                    id:"any",
+                    name:"any",
+                    year:"any",
+                    cover:"any",
+                },
+                user:{
+                    id:"any",
+                    name:"any",
+                    email:"any@any.com",
+                }
+            },
+        ]
+    }
+
+    const playlistRepository=new PlaylistRepositoryMemory();
+    
+    const songRepository=new SongRepositoryMemory()
+    await songRepository.create(INITIAL_VALUES.songs[0] as Song)
+    await songRepository.create(INITIAL_VALUES.songs[1] as Song)
+    
+    const createPlaylist=new CreatePlaylist(
+        playlistRepository,
+        songRepository,
+        identifier
+    );
+    
+    it('should create playlist',async ()=>{
+        const playlist=await createPlaylist.execute(INITIAL_VALUES);
+
+        assert.deepEqual(playlist.id,"1")
+        assert.deepEqual(playlist.date.toDateString(),new Date().toDateString())
+    })
+    
+    it("throw error when some song to list not found", async ()=>{
+        await assert.rejects(async ()=>{
+            await createPlaylist.execute(
+                {
+                    ...INITIAL_VALUES,
+                    songs: [{...INITIAL_VALUES.songs[0],id:"wrongId"}],
+                }
+            );
+            
+        },new SongNotFound())
+    })
+})
---
+++ tests/Playlist.test.ts
@@ -0,0 +1,106 @@
+import assert from 'assert'
+import { Playlist } from 'domain/entities/Playlist'
+import { EmptyPlaylist } from 'domain/errors/EmptyPlaylist'
+import { FieldMissing } from 'domain/errors/FieldMissing'
+
+describe("Playlist",async ()=>{
+    const INITIAL_VALUES:Playlist.Props={
+        id: "any",
+        title: "any",
+        user: {
+            id:"any",
+            name:"any",
+            email:"any"
+        },
+        songs:[
+            {
+                id:"any",
+                title:"any",
+                category:{
+                    id:"any",
+                    name:"any",
+                },
+                duration:1.0,
+                pathSongFile:"any",
+                artist:{
+                    id:"any",
+                    name:"any",
+                    picture:"any"
+                },
+                album:{
+                    id:"any",
+                    name:"any",
+                    year:"any",
+                    cover:"any",
+                },
+                user:{
+                    id:"any",
+                    name:"any",
+                    email:"any@any.com",
+                }
+            },
+
+            {
+                id:"any",
+                title:"any2",
+                category:{
+                    id:"any",
+                    name:"any",
+                },
+                duration:2.0,
+                pathSongFile:"any",
+                artist:{
+                    id:"any",
+                    name:"any",
+                    picture:"any"
+                },
+                album:{
+                    id:"any",
+                    name:"any",
+                    year:"any",
+                    cover:"any",
+                },
+                user:{
+                    id:"any",
+                    name:"any",
+                    email:"any@any.com",
+                }
+            },
+        ]
+    }
+
+    it("should create playlist",async ()=>{
+        const playlist=new Playlist(INITIAL_VALUES)
+        
+        assert.deepEqual(playlist.id,"any")
+        assert.deepEqual(playlist.title,"any")
+        assert.deepEqual(playlist.user.id,"any")
+        assert.deepEqual(playlist.songs.length,2) 
+    })
+    
+    
+    it("throw error when title is not send",async ()=>{
+        assert.throws(()=>{
+            new Playlist({...INITIAL_VALUES,title:""})},
+            new FieldMissing('Title'))
+    })
+
+    it("throw error when user is not send",async ()=>{
+        assert.throws(()=>{
+            new Playlist({...INITIAL_VALUES,user:null})},
+            new FieldMissing('User'))
+    })
+
+    it("throw error when list song is not send",async ()=>{
+        assert.throws(()=>{
+            new Playlist({...INITIAL_VALUES,songs:null})},
+            new FieldMissing('Songs List'))
+    })
+    
+    it("throw error when empty song list is send",async ()=>{
+        assert.throws(()=>{
+            new Playlist({...INITIAL_VALUES,songs:[]})},
+            new EmptyPlaylist())
+    })
+
+})
