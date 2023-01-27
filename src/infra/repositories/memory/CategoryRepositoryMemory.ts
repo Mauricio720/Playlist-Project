0438f874946839a3042ee30838f94e6cfc737940
Mauricio Ferreira <mauricio-ferreira2015@outlok.com>
2023-01-27 20:32:05 -0300

    feat: category already category validate

--- src/infra/repositories/memory/CategoryRepositoryMemory.ts
+++ src/infra/repositories/memory/CategoryRepositoryMemory.ts
@@ -1,32 +1,39 @@
 import { CategogyRepository } from "application/repositories/CategoryRepository";
 import { Category } from "domain/entities/Category";
 
-export class CategoryRepositoryMemory implements CategogyRepository{
-    private categories:Category[]=[];
-    
-    async list():Promise<Category[]>{
-        return this.categories; 
-    }
-    
-    async create(category: Category): Promise<Category> {
-        this.categories.push(category)
-        return category;
-    }
+export class CategoryRepositoryMemory implements CategogyRepository {
+  private categories: Category[] = [];
 
-    async update(category: Category): Promise<Category> {
-        const filterCategories=this.categories.filter((categoryItem)=>categoryItem.id!==categoryItem.id)
-        filterCategories.push(category)
-        this.categories=filterCategories;
-        return category;
-    }
+  async list(): Promise<Category[]> {
+    return this.categories;
+  }
 
-    async delete(id: string): Promise<void> {
-        const filterCategories=this.categories.filter((categoryItem)=>categoryItem.id!==id)
-        this.categories=filterCategories;
-    }
+  async create(category: Category): Promise<Category> {
+    this.categories.push(category);
+    return category;
+  }
 
-    async findById(id: string):Promise<Category | null>{
-        return this.categories.find((category) => category.id === id) || null;
-    }
+  async update(category: Category): Promise<Category> {
+    const filterCategories = this.categories.filter(
+      (categoryItem) => categoryItem.id !== categoryItem.id
+    );
+    filterCategories.push(category);
+    this.categories = filterCategories;
+    return category;
+  }
 
+  async delete(id: string): Promise<void> {
+    const filterCategories = this.categories.filter(
+      (categoryItem) => categoryItem.id !== id
+    );
+    this.categories = filterCategories;
+  }
+
+  async findById(id: string): Promise<Category | null> {
+    return this.categories.find((category) => category.id === id) || null;
+  }
+
+  async findByName(name: string): Promise<Category | null> {
+    return this.categories.find((category) => category.name === name) || null;
+  }
 }
