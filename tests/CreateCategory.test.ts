32dbd09091c523163941d209e5172dc29c167c0a
Mauricio Ferreira <mauricio-ferreira2015@outlok.com>
2023-01-27 20:31:41 -0300

    test: throw already category

--- tests/CreateCategory.test.ts
+++ tests/CreateCategory.test.ts
@@ -1,24 +1,30 @@
-import { CreateCategory } from "application/useCases/CreateCategory"
-import assert from "assert"
-import { CategoryRepositoryMemory } from "infra/repositories/memory/CategoryRepositoryMemory"
-import { Identifier } from "infra/security/Identifier"
+import { CreateCategory } from "application/useCases/CreateCategory";
+import assert from "assert";
+import { AlreadyExists } from "domain/errors/AlreadyExists";
+import { CategoryRepositoryMemory } from "infra/repositories/memory/CategoryRepositoryMemory";
+import { Identifier } from "infra/security/Identifier";
 
+describe("Create New Category", () => {
+  const identifier: Identifier = {
+    createId() {
+      return "1";
+    },
+  };
 
-describe("Create New Category",()=>{
-    const identifier:Identifier={
-        createId(){
-            return "1"
-        }
-    }
+  const categoryRepository = new CategoryRepositoryMemory();
+  const createCategory = new CreateCategory(identifier, categoryRepository);
 
-    it("should create new category",async ()=>{
-        const categoryRepository=new CategoryRepositoryMemory()
-        const createCategory=new CreateCategory(identifier,categoryRepository)
-        const category=await createCategory.execute('any','any')
-        
-        assert.deepEqual(category.id,'1')
-        assert.deepEqual(category.name,'any')
-        assert.deepEqual(category.icon,'any')
-    })
-    
-})
+  it("should create new category", async () => {
+    const category = await createCategory.execute("any", "any");
+
+    assert.deepEqual(category.id, "1");
+    assert.deepEqual(category.name, "any");
+    assert.deepEqual(category.icon, "any");
+  });
+
+  it("throw category already exists", async () => {
+    await assert.rejects(async () => {
+      await createCategory.execute("any", "any");
+    }, new AlreadyExists("Category"));
+  });
+});
